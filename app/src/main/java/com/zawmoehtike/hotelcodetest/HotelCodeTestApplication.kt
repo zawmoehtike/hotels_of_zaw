013269c9c184372ccfc0cd6cec1cd75bd42df574
Zaw Moe Htike <uchihamoee@gmail.com>
2023-04-23 09:13:26 +0630

    import core dependencies

---
+++ app/src/main/java/com/zawmoehtike/hotelcodetest/HotelCodeTestApplication.kt
@@ -0,0 +1,8 @@
+package com.zawmoehtike.hotelcodetest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HotelCodeTestApplication: Application() {
+}
